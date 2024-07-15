17d6054f7f1c867d23349b4041d0598b9c00d9ec
Allan Clements <w.github@kamalisk.com>
2024-07-15 15:30:21 +0000

    include side schemes in text export

--- src/AppBundle/Model/SlotCollectionDecorator.php
+++ src/AppBundle/Model/SlotCollectionDecorator.php
@@ -4,17 +4,17 @@ namespace AppBundle\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
 /**
- * Decorator for a collection of SlotInterface 
+ * Decorator for a collection of SlotInterface
  */
 class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterface
 {
 	protected $slots;
-	
+
 	public function __construct(\Doctrine\Common\Collections\Collection $slots)
 	{
 		$this->slots = $slots;
 	}
-	
+
 	public function add($element)
 	{
 		return $this->slots->add($element);
@@ -24,38 +24,38 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 	{
 		return $this->slots->removeElement($element);
 	}
-	
+
 	public function count($mode = null)
 	{
 		return $this->slots->count($mode);
 	}
-	
+
 	public function getIterator()
 	{
 		return $this->slots->getIterator();
 	}
-	
+
 	public function offsetExists($offset)
 	{
 		return $this->slots->offsetExists($offset);
 	}
-	
+
 	public function offsetGet($offset)
 	{
 		return $this->slots->offsetGet($offset);
 	}
-	
+
 	public function offsetSet($offset, $value)
 	{
 		return $this->slots->offsetSet($offset, $value);
 	}
-	
+
 	public function offsetUnset($offset)
 	{
 		return $this->slots->offsetUnset($offset);
 	}
-	
-	public function countCards() 
+
+	public function countCards()
 	{
 		$count = 0;
 		foreach($this->slots as $slot) {
@@ -63,7 +63,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return $count;
 	}
-	
+
 	public function getIncludedPacks() {
 		$packs = [];
 		foreach ( $this->slots as $slot ) {
@@ -75,7 +75,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 					'nb' => 0
 				];
 			}
-			
+
 			$nbpacks = ceil($slot->getQuantity() / $card->getQuantity());
 			if($packs[$pack->getPosition()]['nb'] < $nbpacks) {
 				$packs[$pack->getPosition()]['nb'] = $nbpacks;
@@ -84,9 +84,9 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		ksort($packs);
 		return array_values($packs);
 	}
-	
+
 	public function getSlotsByType($excludeWeakness=0) {
-		$slotsByType = [ 'upgrade' => [], 'ally' => [], 'support' => [], 'event' => [], 'resource' => []];
+		$slotsByType = [ 'upgrade' => [], 'ally' => [], 'support' => [], 'event' => [], 'resource' => [], 'player_side_scheme' => []];
 		foreach($this->slots as $slot) {
 			if(array_key_exists($slot->getCard()->getType()->getCode(), $slotsByType)) {
 				$slotsByType[$slot->getCard()->getType()->getCode()][] = $slot;
@@ -94,7 +94,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return $slotsByType;
 	}
-	
+
 	public function getSlotsBySubType($subtype) {
 		$slots = [];
 		foreach($this->slots as $slot) {
@@ -104,7 +104,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return $slots;
 	}
-	
+
 	public function getCountByType() {
 		$countByType = [ 'upgrade' => 0, 'ally' => 0, 'support' => 0, 'event' => 0, 'resource' => 0 ];
 		foreach($this->slots as $slot) {
@@ -129,7 +129,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return new SlotCollectionDecorator(new ArrayCollection($drawDeck));
 	}
-	
+
 	public function getCopiesAndDeckLimit()
 	{
 		$copiesAndDeckLimit = [];
@@ -147,7 +147,7 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return $copiesAndDeckLimit;
 	}
-	
+
 	public function getSlots()
 	{
 		return $this->slots;
@@ -176,5 +176,5 @@ class SlotCollectionDecorator implements \AppBundle\Model\SlotCollectionInterfac
 		}
 		return $arr;
 	}
-	
+
 }

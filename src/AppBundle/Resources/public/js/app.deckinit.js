cdb6d78bb0ede69810ad6f509c5c434d3fa70321
Allan Clements <w.github@kamalisk.com>
2026-04-02 14:40:53 +0000

    add format select to hero page

--- src/AppBundle/Resources/public/js/app.deckinit.js
+++ src/AppBundle/Resources/public/js/app.deckinit.js
@@ -3,22 +3,34 @@
 	var deck_init_config = null;
 
 	deck_init.update_build_init = function(){
-		$("#all_inv").toggleClass("hidden");
-		$("#my_inv").toggleClass("hidden");
-		if ($("#all_inv").hasClass("hidden")){
+
+		if ($("#deck_init_all").val() == "your"){
 			deck_init_config.all = false;
+			$("#all_inv").addClass("hidden");
+			$("#my_inv").removeClass("hidden");
 		} else {
 			deck_init_config.all = true;
+			$("#all_inv").removeClass("hidden");
+			$("#my_inv").addClass("hidden");
 		}
+
+		if ($("#deck_init_format").val() == "current") {
+			deck_init_config.format = "current";
+			$("div[data-legacy='1']").addClass("hidden");
+		} else {
+			deck_init_config.format = "legacy";
+			$("div[data-legacy='1']").removeClass("hidden");
+		}
+
 		if (localStorage) {
 			localStorage.setItem('ui.deck.init', JSON.stringify(deck_init_config));
 		}
 	}
-	
-	deck_init.on_all_loaded = function() {	
+
+	deck_init.on_all_loaded = function() {
 		window.alert("rah?");
 	}
-	
+
 	$(document).ready(function(){
 		if (localStorage) {
 			var stored = localStorage.getItem('ui.deck.init');
@@ -29,16 +41,20 @@
 		deck_init_config = _.extend({
 			'all': false,
 		}, deck_init_config || {});
-		
+
 		if (deck_init_config.all){
-			$("#all_inv").removeClass("hidden");
-			$("#my_inv").addClass("hidden");
 			$("#deck_init_all").val("all");
 		} else {
-			$("#all_inv").addClass("hidden");
-			$("#my_inv").removeClass("hidden");
 			$("#deck_init_all").val("your");
 		}
-		
+
+		if (deck_init_config.format == "current"){
+			$("#deck_init_format").val("current");
+		} else {
+			$("#deck_init_format").val("legacy");
+		}
+
+		deck_init.update_build_init();
+
 	});
 })(app.deck_init = {}, jQuery);

68bab932e45d1bee0e9733c0d11ce8afa8d913e6
Allan Clements <w.github@kamalisk.com>
2026-05-09 11:36:28 +0000

    update deck import to parse sets and subtitles for better reliability

--- src/AppBundle/Resources/public/js/ui.deckimport.js
+++ src/AppBundle/Resources/public/js/ui.deckimport.js
@@ -4,38 +4,75 @@ var name_regexp;
 
 ui.on_content_change = function on_content_change(event) {
 	var text = $(content).val(),
-		slots = {}, 
-		hero_code, 
+		slots = {},
+		hero_code,
 		hero_name;
-	
+
 	text.match(name_regexp).forEach(function (token) {
-		var qty = 1, name = token.trim(), card;
+		var qty = 1, name = token.trim(), card, pack_name = '', query;
 		if(token[0] === '(') {
 			return;
 		}
-		if(name.match(/^(\d+)x ([^()]*)/)) {
+		if(name.match(/^(\d+)x\s+(.+)$/)) {
 			qty = parseInt(RegExp.$1, 10);
 			name = RegExp.$2.trim();
 		}
-		console.log(name);
-		if(card = app.data.cards.findOne({ name: name })) {			
-			if (card.type_code == "hero"){
+
+		// Separate check for pack_name in parentheses
+		if(name.match(/^(.+?)\s*\(([^)]+)\)$/)) {
+			name = RegExp.$1.trim();
+			pack_name = RegExp.$2.trim();
+		}
+
+		console.log(name, pack_name);
+
+		// assume the first thing we find is gonna be a hero
+		if (!hero_code) {
+			query = { name: name, type_code: 'hero' };
+			if(pack_name) {
+				query.pack_name = pack_name;
+			}
+			if(card = app.data.cards.findOne(query)) {
 				hero_code = card.code;
 				hero_name = card.name;
-			} else {
-				slots[card.code] = qty;	
+			}
+		} else {
+			// Parse title and subtitle from format "Title: Subtitle"
+			var title = name, subtitle = '';
+			if(name.match(/^(.+?)\s*:\s*(.+)$/)) {
+				title = RegExp.$1.trim();
+				subtitle = RegExp.$2.trim();
+			}
+
+			// first check with title and subtitle if subtitle exists
+			if(subtitle) {
+				query = { name: title, subtitle: subtitle, type_code: { $ne: 'hero' } };
+				if(pack_name) {
+					query.pack_name = pack_name;
+				}
+				card = app.data.cards.findOne(query);
+			}
+
+			// if not found, check just the name
+			if(!card) {
+				query = { name: name, type_code: { $ne: 'hero' } };
+				if(pack_name) {
+					query.pack_name = pack_name;
+				}
+				card = app.data.cards.findOne(query);
+			}
+
+			if(card) {
+				slots[card.code] = qty;
 			}
 		}
-		else {
-			console.log('rejecting string ['+name+']');
-		}
 	})
-	
+
 	if (!hero_code){
 		window.alert("Unable to locate hero");
 		return;
 	}
-	
+
 	app.deck.init({
 		hero_code: hero_code,
 		hero_name: hero_name,
